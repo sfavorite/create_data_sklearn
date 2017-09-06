5056c9a321edf95aa75693c87768218eb81df61d
Scot Favorite <scot.favorite@gmail.com>
2017-09-06 11:08:07 -0400

    Add modeling and predictions

--- create_data.py
+++ create_data.py
@@ -3,9 +3,10 @@
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 # Create a simulated feature matrix and output vector with 100 samples,
-features, output = make_classification(n_samples = 100,
+X, y = make_classification(n_samples = 100,
                                        # ten features
                                        n_features = 10,
                                        # five features that actually predict the output's classes
@@ -17,3 +18,11 @@ features, output = make_classification(n_samples = 100,
                                        # with 20% of observations in the first class, 30% in the second class,
                                        # and 50% in the third class. ('None' makes balanced classes)
                                        weights = [.2, .3, .8])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+print(np.mean(predictions == y_test) * 100)
