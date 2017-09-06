34196ed043c1eef24f9ed5eeb54b4f25ce0f09df
Scot Favorite <scot.favorite@gmail.com>
2017-09-06 10:50:05 -0400

    Initial commit

---
+++ create_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from sklearn.datasets import make_classification
+import pandas as pd
+
+# Create a simulated feature matrix and output vector with 100 samples,
+features, output = make_classification(n_samples = 100,
+                                       # ten features
+                                       n_features = 10,
+                                       # five features that actually predict the output's classes
+                                       n_informative = 5,
+                                       # five features that are random and unrelated to the output's classes
+                                       n_redundant = 5,
+                                       # three output classes
+                                       n_classes = 3,
+                                       # with 20% of observations in the first class, 30% in the second class,
+                                       # and 50% in the third class. ('None' makes balanced classes)
+                                       weights = [.2, .3, .8])
